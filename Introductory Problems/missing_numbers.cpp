efa1bbff45469a27e1eb0cc7939cba243af1fd46
Kritika Rag <kritikarag.kr@gmail.com>
2023-12-28 14:22:30 +0530

    modified:   Introductory Problems/missing_numbers.cpp

--- Introductory Problems/missing_numbers.cpp
+++ Introductory Problems/missing_numbers.cpp
@@ -1,20 +1,49 @@
 // https://cses.fi/problemset/task/1083
 
-#include<bits/stdc++.h>
-#include<iostream>
+#include <bits/stdc++.h>
+#include <string>
 using namespace std;
+using ll = long long int;
+using pr = pair<ll, ll>;
+using umll = unordered_map<ll, ll>;
+using omll = map<ll, ll>;
+using umcl = unordered_map<char, ll>;
+using omcl = map<char, ll>;
+using qc = queue<char>;
+using qi = queue<ll>;
+using qp = queue<pair<ll, ll>>;
+using v1i = vector<ll>;
+using v1c = vector<char>;
+using v1b = vector<bool>;
+using v2i = vector<vector<ll>>;
+using v2c = vector<vector<char>>;
+using v1b = vector<bool>;
+using v2b = vector<vector<bool>>;
+using v1p = vector<pair<ll, ll>>;
+using maxpq = priority_queue<ll>;
+using minpq = priority_queue<ll, vector<ll>, greater<ll>>;
+using maxpairpq = priority_queue<pair<ll, ll>>;
+using minpairpq = priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>>;
+#define all(vec) vec.begin(), vec.end();
+#define forn(i, x, n) for (ll i = ll(x); i < ll(n); i++)
 
-int main(){
-    long long n;
-    cin>>n;
+long long int mod = 1e9 + 7;
 
-    long long sum = 0;
-    for(int i=0;i<n-1;i++){
-        int x;
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll sum = 0;
+    forn(i,0,n-1){
+        ll x;
         cin>>x;
         sum+=x;
     }
+    ll missing = (n*(n+1))/2 - sum;
+    cout<<missing<<endl;
+}
 
-    long long ans = (n*(n+1))/2 - sum;
-    cout<<ans<<endl;
+int main()
+{
+    solve();
 }
