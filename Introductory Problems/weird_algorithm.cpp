b795d94a2a9ee1d0047f1391bed4f16400e353b8
Kritika Rag <kritikarag.kr@gmail.com>
2023-12-28 14:17:30 +0530

    modified:   Introductory Problems/weird_algorithm.cpp

--- Introductory Problems/weird_algorithm.cpp
+++ Introductory Problems/weird_algorithm.cpp
@@ -1,21 +1,55 @@
 // https://cses.fi/problemset/task/1068
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <string>
 using namespace std;
+using ll = long long int;
+using pr = pair<ll, ll>;
+using umll = unordered_map<ll, ll>;
+using omll = map<ll, ll>;
+using umcl = unordered_map<char, ll>;
+using omcl = map<char, ll>;
+using qc = queue<char>;
+using qi = queue<ll>;
+using qp = queue<pair<ll, ll>>;
+using v1i = vector<ll>;
+using v1c = vector<char>;
+using v1b = vector<bool>;
+using v2i = vector<vector<ll>>;
+using v2c = vector<vector<char>>;
+using v1b = vector<bool>;
+using v2b = vector<vector<bool>>;
+using v1p = vector<pair<ll, ll>>;
+using maxpq = priority_queue<ll>;
+using minpq = priority_queue<ll, vector<ll>, greater<ll>>;
+using maxpairpq = priority_queue<pair<ll, ll>>;
+using minpairpq = priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>>;
+#define all(vec) vec.begin(), vec.end();
+#define forn(i, x, n) for (ll i = ll(x); i < ll(n); i++)
 
-int main(){
-    long long n;
-    cin>>n;
-    while(n!=1){
-        if(n%2==0){
-            cout<<n<<" ";
-            n/=2;
+long long int mod = 1e9 + 7;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    while (n != 1)
+    {
+        cout << n << " ";
+        if (n & 1)
+        {
+            n = n * 3 + 1;
         }
-        else{
-            cout<<n<<" ";
-            n= n*3+1;
+        else
+        {
+            n /= 2;
         }
     }
-    cout<<1<<endl;
-    return 0;   
+    cout << 1 << endl;
+}
+
+int main()
+{
+    solve();
 }
